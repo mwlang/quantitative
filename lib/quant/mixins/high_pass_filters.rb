10ab1f84fec30bc4c7ea9261ddb3c8a3c3967d89
mwlang <mwlang@cybrains.net>
2024-03-09 15:25:06 -0500

    High Pass Filters documentation

--- lib/quant/mixins/high_pass_filters.rb
+++ lib/quant/mixins/high_pass_filters.rb
@@ -2,13 +2,42 @@
 
 module Quant
   module Mixins
+    # The following are high pass filters that are used to remove low frequency
+    # components from a time series. In simple terms, a high pass filter
+    # allows signals above a certain frequency (the cutoff frequency) to
+    # pass through relatively unaffected, while attenuating or blocking
+    # signals below that frequency.
+    #
+    # HighPass Filters are “detrenders” because they attenuate low frequency components
+    # One pole HighPass and SuperSmoother does not produce a zero mean because low
+    # frequency spectral dilation components are "leaking" through The one pole
+    # HighPass Filter response
+    #
+    # == Experimental
+    # Across the various texts and papers, Ehlers presents varying implementations
+    # of high-pass filters.  I believe the two pole high-pass filter is the most
+    # consistently presented while the one pole high-pass filter has been presented
+    # in a few different ways.  In some implementations, alpha is based on simple
+    # bars/lag while others use alpha based on phase/trigonometry.  I have not been
+    # able to reconcile the differences and have not been able to find a definitive
+    # source for the correct implementation and do not know enough math to reason
+    # these out mathematically nor do I possess an advanced understanding of the
+    # fundamentals around digital signal processing.  As such, the single-pole
+    # high-pass filters in this module are marked as experimental and may be incorrect.
     module HighPassFilters
-      # HighPass Filters are “detrenders” because they attenuate low frequency components
-      # One pole HighPass and SuperSmoother does not produce a zero mean because low
-      # frequency spectral dilation components are “leaking” through The one pole
-      # HighPass Filter response
-      def two_pole_high_pass_filter(source, period:, previous:)
+      # A two-pole high-pass filter is a more advanced filtering technique
+      # used to remove low-frequency components from financial time series
+      # data, such as stock prices or market indices.
+      #
+      # Similar to a single-pole high-pass filter, a two-pole high-pass filter
+      # is designed to attenuate or eliminate slow-moving trends or macroeconomic
+      # effects from the data while preserving higher-frequency fluctuations.
+      # However, compared to the single-pole filter, the two-pole filter
+      # typically offers a steeper roll-off and better attenuation of lower
+      # frequencies, resulting in a more pronounced emphasis on short-term fluctuations.
+      def two_pole_high_pass_filter(source, period:, previous: :hp)
         raise ArgumentError, "source must be a Symbol" unless source.is_a?(Symbol)
+        raise ArgumentError, "previous must be a Symbol" unless previous.is_a?(Symbol)
 
         alpha = period_to_alpha(period, k: 0.707)
 
@@ -23,6 +52,31 @@ module Quant
         a + b - c
       end
 
+      # A single-pole high-pass filter is used to filter out low-frequency
+      # components from financial time series data. This type of filter is
+      # commonly applied in signal processing techniques to remove noise or
+      # unwanted trends from the data while preserving higher-frequency fluctuations.
+      #
+      # A single-pole high-pass filter can be used to remove slow-moving trends
+      # or macroeconomic effects from the data, focusing instead on short-term
+      # fluctuations or high-frequency trading signals. By filtering out
+      # low-frequency components, traders aim to identify and exploit more
+      # immediate market opportunities, such as short-term price movements
+      # or momentum signals.
+      #
+      # The implementation of a single-pole high-pass filter in algorithmic
+      # trading typically involves applying a mathematical formula or algorithm
+      # to the historical price data of a financial instrument. This algorithm
+      # selectively attenuates or removes the low-frequency components of the
+      # data, leaving behind the higher-frequency fluctuations that traders
+      # are interested in analyzing for potential trading signals.
+      #
+      # Overall, single-pole high-pass filters in algorithmic trading are
+      # used as preprocessing steps to enhance the signal-to-noise ratio in
+      # financial data and to extract actionable trading signals from noisy
+      # or cluttered market data.
+      #
+      # == NOTES
       # alpha = (Cosine(.707* 2 * PI / 48) + Sine (.707*360 / 48) - 1) / Cosine(.707*360 / 48);
       # is the same as the following:
       # radians = Math.sqrt(2) * Math::PI / period
@@ -30,10 +84,7 @@ module Quant
       def high_pass_filter(source, period:, previous: :hp)
         Quant.experimental("This method is unproven and may be incorrect.")
         raise ArgumentError, "source must be a Symbol" unless source.is_a?(Symbol)
-
-        v0 = p0.send(source)
-        v1 = p1.send(previous)
-        v2 = p2.send(previous)
+        raise ArgumentError, "previous must be a Symbol" unless previous.is_a?(Symbol)
 
         radians = Math.sqrt(2) * Math::PI / period
         a = Math.exp(-radians)
@@ -43,7 +94,35 @@ module Quant
         c3 = -a**2
         c1 = (1 + c2 - c3) / 4
 
-        (c1 * (v0 - (2 * v1) + v2)) + (c2 * v1) + (c3 * v2)
+        v0 = p0.send(source)
+        v1 = p1.send(source)
+        v2 = p2.send(source)
+        f1 = p1.send(previous)
+        f2 = p2.send(previous)
+
+        (c1 * (v0 - (2 * v1) + v2)) + (c2 * f1) + (c3 * f2)
+      end
+
+      # HPF = (1 − α/2)2 * (Price − 2 * Price[1] + Price[2]) + 2 * (1 − α) * HPF[1] − (1 − α)2 * HPF[2];
+      # High Pass Filter presented in Ehlers Cybernetic Analysis for Stocks and Futures Equation 2.7
+      def hpf2(source, period:, previous:)
+        Quant.experimental("This method is unproven and may be incorrect.")
+        raise ArgumentError, "source must be a Symbol" unless source.is_a?(Symbol)
+        raise ArgumentError, "previous must be a Symbol" unless previous.is_a?(Symbol)
+
+        alpha = period_to_alpha(period, k: 1.0)
+        v0 = p0.send(source)
+        v1 = p1.send(source)
+        v2 = p1.send(source)
+
+        f1 = p1.send(previous)
+        f2 = p2.send(previous)
+
+        c1 = (1 - alpha / 2)**2
+        c2 = 2 * (1 - alpha)
+        c3 = (1 - alpha)**2
+
+        (c1 * (v0 - (2 * v1) + v2)) + (c2 * f1) - (c3 * f2)
       end
     end
   end
--- spec/lib/quant/mixins/high_pass_filters_spec.rb
+++ spec/lib/quant/mixins/high_pass_filters_spec.rb
@@ -5,6 +5,7 @@ require "spec_helper"
 module HighPassFilterMixinTest
   class TestPoint < Quant::Indicators::IndicatorPoint
     attribute :hp, default: :oc2
+    attribute :hpf2, default: :oc2
     attribute :hp2p, default: :oc2
     attribute :uhp1p, default: :oc2
     attribute :uhp2p, default: :oc2
@@ -18,8 +19,9 @@ module HighPassFilterMixinTest
     end
 
     def compute
-      p0.hp = high_pass_filter(:input, period: 3).round(3)
-      p0.uhp1p = universal_one_pole_high_pass(:input, period: 3, previous: :uhp1p).round(3)
+      p0.hp = high_pass_filter(:input, period: 14, previous: :hp).round(3)
+      p0.hpf2 = hpf2(:input, period: 14, previous: :hpf2).round(3)
+      p0.uhp1p = universal_one_pole_high_pass(:input, period: 14, previous: :uhp1p).round(3)
 
       p0.hp2p = two_pole_high_pass_filter(:input, period: 3, previous: :hp2p)
       p0.uhp2p = universal_two_pole_high_pass(:input, period: 3, previous: :uhp2p)
@@ -39,8 +41,9 @@ module HighPassFilterMixinTest
       it { expect(subject.ticks.size).to eq(subject.series.size) }
       it { expect(subject.values.map(&:input)).to eq([3.0, 6.0, 12.0, 24.0]) }
 
-      it { expect(subject.values.map(&:hp)).to    eq([-0.033, 1.648, 2.437, 5.688]) }
-      it { expect(subject.values.map(&:uhp1p)).to eq([-0.804, 1.314, 1.844, 3.898]) }
+      it { expect(subject.values.map(&:uhp1p)).to eq([1.885, 3.627, 7.164, 14.271]) }
+      it { expect(subject.values.map(&:hp)).to    eq([2.560, 4.370, 6.874, 11.564]) }
+      it { expect(subject.values.map(&:hpf2)).to  eq([2.586, 4.217, 8.256, 16.665]) }
 
       it { expect(subject.values.map{ |m| m.hp2p.round(4) }).to  eq([0.2643, 0.8424, 0.8945, 1.7171]) }
       it { expect(subject.values.map{ |m| m.uhp2p.round(4) }).to eq([0.2636, 0.8421, 0.8941, 1.7165]) }
@@ -50,12 +53,12 @@ module HighPassFilterMixinTest
       let(:series) { Quant::Series.new(symbol: "HP", interval: "1d") }
 
       context "single-pole high-pass filter" do
-        [[1, -0.011, -0.268],
-         [2, 0.549, 0.438],
-         [3, 0.539, 0.249],
-         [4, 0.942, 0.299],
-         [5, 1.009, 0.286],
-         [6, 1.337, 0.289]].each do |n, expected, expected_universal|
+        [[1, 0.853, 0.628],
+         [2, 1.456, 1.209],
+         [3, 1.562, 1.574],
+         [4, 1.389, 1.803],
+         [5, 1.094, 1.947],
+         [6, 0.777, 2.038]].each do |n, expected, expected_universal|
           dataset = (1..n).to_a
 
           it "is #{expected.inspect} for :hp when series: is #{dataset.inspect}" do
@@ -102,8 +105,8 @@ module HighPassFilterMixinTest
       it { expect(subject.ticks.size).to eq(subject.series.size) }
       it { expect(subject.values.map(&:input)).to be_all(5.0) }
 
-      it { expect(subject.values.map(&:hp).uniq.take(5)).to eq([-0.055, 1.381, 0.655, 1.34, 0.833]) }
-      it { expect(subject.values.map(&:uhp1p).take(5)).to eq([-1.34, 0.359, -0.096, 0.026, -0.007]) }
+      it { expect(subject.values.map(&:hp).uniq.take(5)).to eq([4.267, 3.641, 2.775, 1.909, 1.17]) }
+      it { expect(subject.values.map(&:uhp1p).take(5)).to   eq([3.142, 1.974, 1.240, 0.779, 0.489]) }
 
       it { expect(subject.values.map{ |m| m.hp2p.round(4) }.take(5)).to  eq([0.4404, 0.0388, 0.0026, 0.0002, 0.0]) }
       it { expect(subject.values.map{ |m| m.uhp2p.round(4) }.take(5)).to eq([0.4394, 0.0386, 0.0026, 0.0002, 0.0]) }
